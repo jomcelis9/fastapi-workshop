1ce9c144084f1c0842530ad068e210d6ef41b2a4
ArJSarmiento <rneljan@gmail.com>
2024-11-21 14:15:22 +0800

    feat: Path Parameters and Query Parameters**

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
