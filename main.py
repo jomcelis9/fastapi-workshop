ba0bb3114bc0f7da94e6d79b74b3ec200729c000
ArJSarmiento <rneljan@gmail.com>
2024-11-21 14:37:45 +0800

    feat: Request Body and Validation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -11,3 +13,13 @@ def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: Optional[str] = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
