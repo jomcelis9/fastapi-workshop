370064c51ba3aae58c1766b9b070541de6fbc097
ArJSarmiento <rneljan@gmail.com>
2024-11-23 13:55:52 +0800

    feat: added dependency injection

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Query, Path
 from models.constants import Message
 from models.item import Item, ItemOut
-from fastapi import Query, Path
 from http import HTTPStatus
+from typing import List
 
 app = FastAPI()
 
@@ -13,6 +13,13 @@ def read_root():
     return {"Hello": "World"}
 
 
+async def common_parameters(
+    q: str | None = Query(..., description="Query string"),
+    item_id: int = Path(..., description="The ID of the item to get"),
+):
+    return {"q": q, "item_id": item_id}
+
+
 @app.get(
     "/items/{item_id}",
     response_model=ItemOut,
@@ -20,22 +27,38 @@ def read_root():
     response_model_exclude_unset=True,
     responses={
         HTTPStatus.NOT_FOUND: {"model": Message, "description": "Item not found"},
-        HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Message, "description": "Internal Server Error"},
+        HTTPStatus.INTERNAL_SERVER_ERROR: {
+            "model": Message,
+            "description": "Internal Server Error",
+        },
     },
     summary="Get Item",
     description="Get item details for a product",
 )
-async def read_item(
-    item_id: int = Path(..., description="The ID of the item to get"),
-    q: str = Query(..., description="Query string"),
-):
-    print(item_id, q)
+async def read_item(params: dict = Depends(common_parameters)):
+    print(params)
     return ItemOut(
-        name=str(item_id),
+        name=str(params["item_id"]),
         price=100,
-        description=q,
+        description=params["q"],
         created_at=datetime.now(),
     )
+
+
+@app.get(
+    "/items_list/{item_id}",
+    dependencies=[Depends(common_parameters)],
+    response_model=List[ItemOut],
+)
+async def read_item_list(params: dict = Depends(common_parameters)):
+    return [
+        ItemOut(
+            name=str(params["item_id"]),
+            price=100,
+            description=params["q"],
+            created_at=datetime.now(),
+        )
+    ]
 
 
 @app.post(
@@ -44,7 +67,10 @@ async def read_item(
     response_model_exclude_none=True,
     response_model_exclude_unset=True,
     responses={
-        HTTPStatus.INTERNAL_SERVER_ERROR: {"model": Message, "description": "Internal Server Error"},
+        HTTPStatus.INTERNAL_SERVER_ERROR: {
+            "model": Message,
+            "description": "Internal Server Error",
+        },
     },
     summary="Create Item",
     description="Create item details for a product",
--- models/item.py
+++ models/item.py
@@ -2,10 +2,12 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional
 
+
 class Item(BaseModel):
     name: str
     price: float
     description: Optional[str] = None
+
 
 class ItemOut(Item):
     created_at: datetime
